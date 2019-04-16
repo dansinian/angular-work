ded0c7df141995b90bdf747c5eca3ba367062a41
su.dan <su.dandan@dian-tong.com>
2019-04-16 16:39:17 +0800

    update code

--- src/app/admin/login.component.ts
+++ src/app/admin/login.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpRequest, HttpResponse, HttpEvent, HttpEventType } from '@angular/common/http';
 import { AppService } from '../app.service';
 import { Router } from '@angular/router';
 import { Admin } from '../entity/admin';
-import { stagger } from '@angular/animations/src/animation_metadata';
+import { FileUploader } from 'ng2-file-upload';
+
+import { NzMessageService, UploadFile, UploadXHRArgs  } from 'ng-zorro-antd';
+import { filter } from 'rxjs/operators';
+import { forkJoin } from 'rxjs';
 
 
 @Component({
@@ -14,14 +18,63 @@ export class LoginComponent implements OnInit {
   basePath;
   admin: Admin;
   sendData;
+  uploader;  //上传文件
+  aaa;
+  uploading = false;
+  fileList: UploadFile[] = [];
 
-  constructor(private httpClient: HttpClient, private appService: AppService, private route: Router) { 
+  beforeUpload = (file: UploadFile): boolean => {
+    console.log(file);
+    this.fileList = this.fileList.concat(file);
+    return false;
+  };
+
+  constructor(private httpClient: HttpClient, private appService: AppService, private route: Router,  private msg: NzMessageService) { 
     this.basePath = this.appService.getBasePath();
     this.admin = {account: '', password: '', type: '', flagLogin: true};
   }
 
   ngOnInit() {
-    
+    this.uploader = new FileUploader({ 
+      url: this.basePath + "/uploadFile", 
+      method: "POST", 
+      itemAlias: "uploadedfile",
+      autoUpload: false,
+     });  
+  }
+
+  handleUpload(): void {
+    const formData = new FormData();
+    // tslint:disable-next-line:no-any
+    this.fileList.forEach((file: any) => {
+      formData.append('files[]', file);
+    });
+    this.uploading = true;
+
+    console.log(formData, this.fileList);
+
+    // You can use any AJAX library you like
+    const req = new HttpRequest('POST', 'https://jsonplaceholder.typicode.com/posts/', formData, {
+      // reportProgress: true
+    });
+    console.log(req);
+    // this.httpClient.request(req).pipe(filter(e => e instanceof HttpResponse)).subscribe(
+    //     () => {
+    //       this.uploading = false;
+    //       this.fileList = [];
+    //       this.msg.success('upload successfully.');
+    //     },
+    //     () => {
+    //       this.uploading = false;
+    //       this.msg.error('upload failed.');
+    //     }
+    //   );
+  }
+
+  selectedFileOnChanged(event) {
+    console.log(event);
+    console.log(event.target.value);
+    console.log(this.aaa);
   }
 
   //登录
